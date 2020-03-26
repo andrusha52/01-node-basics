a2b8ef00332c40f0ab83f93cdcac9a12b8674593
andrusha52 <andrushaes@gmail.com>
2020-03-26 14:15:13 +0200

    add hw

---
+++ index.js
@@ -0,0 +1,30 @@
+const argv = require('yargs').argv;
+const contacts = require('./contacts')
+
+
+
+function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+      case 'list':
+        console.table(contacts.listContacts())
+        break;
+  
+      case 'get':
+        console.log(contacts.getContactById(id))
+        //... id
+        break;
+  
+      case 'add':
+          console.log(contacts.addContact(name,email,phone))
+        // ... name email phone
+        break;
+  
+      case 'remove':
+          console.log(contacts.removeContact(id))
+        // ... id
+        break;
+  
+      default:
+        console.warn('\x1B[31m Unknown action type!');
+    }
+  }
